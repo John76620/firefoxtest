debeefc34f579f30cee648996ccadfd72eabbd32
John76620 <68542955+John76620@users.noreply.github.com>
2020-07-20 15:50:45 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import syscoinBot
 
+compteur = 0
 syscoin = syscoinBot.syscoinBot()
-syscoin.connection("sys1qvuncdxpaavf0zu7m96zsr87fsvpnm4zxvpq482")
+while compteur != 20000:
+    syscoin.mine()
+    syscoin.already_mined()
+    if syscoin.already_mined == False:
+        compteur = compteur + 1
+    syscoin.quit()
